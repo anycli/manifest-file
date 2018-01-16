6447783946537612e22995e369ec540b01770135
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-16 15:19:05 -0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,80 @@
-export const add = (a: number, b: number) => a + b
+import * as fs from 'fs-extra'
+import * as loadJSON from 'load-json-file'
+import * as _ from 'lodash'
+import RWLockfile from 'rwlockfile'
+
+const pjson = require('../package.json')
+
+interface Body {
+  [k: string]: any
+}
+
+export default abstract class ManifestFile {
+  protected lock: RWLockfile
+  protected debug: any
+
+  constructor(public type: string, public file: string) {
+    this.debug = require('debug')(`cli:${this.type}`)
+    this.lock = new RWLockfile(this.file)
+  }
+
+  async reset() {
+    await this.lock.add('write')
+    try {
+      this.debug('reset', this.file)
+      await fs.remove(this.file)
+    } finally {
+      await this.lock.remove('write')
+    }
+  }
+
+  protected async get<T>(key: string): Promise<T | undefined>
+  protected async get<T, U>(key: string, secondKey: string): Promise<[T | undefined, U | undefined]>
+  protected async get(key: string, secondKey?: string): Promise<any> {
+    await this.lock.add('read')
+    try {
+      this.debug('get', _.compact([key, secondKey]))
+      const body = await this.read()
+      return secondKey ? [body[key], body[secondKey]] : body[key]
+    } finally {
+      await this.lock.remove('read')
+    }
+  }
+
+  protected async set(...pairs: [string, any][]) {
+    await this.lock.add('write')
+    try {
+      const body = await this.read()
+      for (let [k, v] of pairs) {
+        this.debug('set', k)
+        body[k] = v
+      }
+      await this.write(body)
+    } finally {
+      await this.lock.remove('write')
+    }
+  }
+
+  private async read(): Promise<Body> {
+    const read = async () => {
+      try {
+        return await loadJSON(this.file)
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+          this.debug('manifest not found', this.file)
+        } else {
+          await fs.remove(this.file)
+          throw err
+        }
+      }
+    }
+    const body = (await read()) || {version: pjson.version}
+    if (!body.version) return {version: pjson.version}
+    return body
+  }
+
+  private async write(body: Body): Promise<void> {
+    this.debug('write', this.file)
+    await fs.outputJSON(this.file, body)
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,29 @@
-import {describe, expect} from '@dxcli/dev-test'
+import {describe, expect, it} from '@dxcli/dev-test'
+import * as fs from 'fs-extra'
+import * as path from 'path'
 
-import {add} from '../src'
+import File from '../src'
 
-describe('add', () => {
-  it('1+2=3', () => {
-    expect(add(1, 2)).to.equal(3)
+class ManifestFile extends File {
+  async getAFoo() {
+    return this.get('foo')
+  }
+
+  async setAFoo(n: number) {
+    return this.set(['foo', n])
+  }
+}
+
+const file = path.join(__dirname, '../tmp/manifest.json')
+beforeEach(async () => {
+  await fs.remove(file)
+})
+
+describe('manifest', () => {
+  it('reads and saves', async () => {
+    let a = new ManifestFile('manifestfile', file)
+    await a.setAFoo(101)
+    let b = new ManifestFile('manifestfile', file)
+    await expect(b.getAFoo()).to.eventually.equal(101)
   })
 })
